608e18162848e4f52a22834851dca2bfb70b672a
ibaim <thisisaim@gmail.com>
2021-01-12 08:32:49 -0800

    Added summaries, inputs, outputs to each method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -43,6 +43,16 @@ public class Main {
         print(openCarts);
         return null;
     }
+    /*
+    Summary: This method takes in two times (24 hour format); Opening time and closing time for a particular cart
+            and returns if the food cart is open or not; True -> Open, False -> Close
+
+    Input: Start time and end time as Strings in 24 hour format. ex 09:00, 19:00
+    Output: Boolean value of whether the car is open or not
+
+    The reason there is a check for 24:00 is because in the response from the API, there are some carts that have 24:00
+    as their close time.  24:00 can also be written as 00:00
+    */
 
     public static boolean isOpen(String startTime, String endTime) {
         LocalTime currentTime = LocalTime.now();
@@ -52,25 +62,37 @@ public class Main {
         return currentTime.isBefore(LocalTime.parse(endTime)) && currentTime.isAfter(LocalTime.parse(startTime));
     }
 
+    /*
+    Summary: This method does not take in any arguments and returns the day of the week e.g. Monday, Tuesday, etc...
+
+    Input: None
+    Output: Boolean value of the day of the week (Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday)
+     */
     public static String getDayOfWeek() {
         LocalDate currentDate = LocalDate.now();
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("EEEE");
         return currentDate.format(dtf);
     }
 
+    /*
+    Summary: Prints the list of open food carts (name and location) 10 at a time.  If there are more than 10 carts in the list, it prompts
+    the user to hit a key to continue to see the list.  This is to avoid overwhelming the user.
+
+    Input: List of JSONObjects that contain all the food carts that are open.
+    Output: Prints all the food carts out to the console that are open.
+     */
     public static void print(List<JSONObject> openCarts) {
-        int n = openCarts.size();
         int count = 10;
-        if (n <= count) {
+        if (openCarts.size() <= count) {
             for (JSONObject cart : openCarts) {
                 System.out.println(cart.getString("applicant"));
                 System.out.println(cart.getString("location"));
                 System.out.println();
             }
         } else {
-            for (int i = 1; i < n+1; i++) {
-                System.out.println(openCarts.get(i-1).getString("applicant"));
-                System.out.println(openCarts.get(i-1).getString("location"));
+            for (int i = 1; i < openCarts.size() + 1; i++) {
+                System.out.println(openCarts.get(i - 1).getString("applicant"));
+                System.out.println(openCarts.get(i - 1).getString("location"));
                 System.out.println();
 
                 if (i % 10 == 0) {
@@ -79,6 +101,11 @@ public class Main {
             }
         }
     }
+    /*
+    Summary: Prompts the user on the console to hit a key, to continue
+    Input: None
+    Output: None
+     */
         public static void promptEnter(){
             System.out.println("Press \"ENTER\" to continue");
             Scanner scanner = new Scanner(System.in);
